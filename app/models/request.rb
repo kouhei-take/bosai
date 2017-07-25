39653b559051c005091eae58a078c7a725d2635b
kouhei <syuzo7947@gmail.com>
2017-07-25 19:11:17 +0900

    Request Show Page

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,7 @@
 class Request < ApplicationRecord
   belongs_to :user
   has_many :items_requests
+  has_many :messages
   has_many :items, through: :items_requests
 
   accepts_nested_attributes_for :items_requests ##############
